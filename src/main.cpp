2b095ea8487ea53bd0208995d60b9802e025ac85
Корниенко Антон <111607633+AntoNVeK@users.noreply.github.com>
2024-09-23 00:31:51 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "./headers/ManagerShips.h"
+#include "./headers/Table.h"
 
 
 int main()
 {
-    ManagerShips manager(5, {ONE, TWO, TWO, THREE, FOUR});
-    
-    Ship& element = manager[1];
-    element.shoot(1);
-    const std::vector<States>& segments = element.GetSegments();
-    for (std::size_t i = 0; i < segments.size(); i++)
-    {
-        std::cout << segments[i] << "\n";
-    }
+    ManagerShips manager({ONE, TWO, TWO, THREE, FOUR});
+    Table table(10, 10, manager);
     return 0;
 }
