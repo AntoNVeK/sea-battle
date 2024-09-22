536152ac93de3821ad80b73accdd538427899fe1
Корниенко Антон <111607633+AntoNVeK@users.noreply.github.com>
2024-09-22 19:02:33 +0300

    Update Table.h

--- src/headers/Table.h
+++ src/headers/Table.h
@@ -1,7 +1,9 @@
 #ifndef TABLE_H
 #define TABLE_H
 #include "ManagerShips.h"
-
+#include <map>
+#include <functional>
+#include <iostream>
 enum CellState
 {
     UNKNOWN,
@@ -10,21 +12,27 @@ enum CellState
 };
 
 
+typedef struct Coords
+{
+    int x;
+    int y;
+}Coords;
+
 
 class Table
 {
 private:
     std::size_t width;
     std::size_t height;
-    std::vector<std::vector<CellState>> cells_;
     ManagerShips& manager;
-
+    std::vector<std::vector<CellState>> cells_;
+    std::map<std::reference_wrapper<Ship>, std::vector<Coords>> coords_ships;
 
 
 public:
-    Table(std::size_t x, std::size_t y);
-    Table();
-    ~Table();
+    Table(ManagerShips& manager);
+    Table(int x, int y, ManagerShips& manager);
+    ~Table() {};
 
 
     Table(const Table &other);
@@ -33,10 +41,21 @@ public:
     Table& operator=(const Table &other);
     Table& operator=(Table &&other);
 
+    
+
+    const int& GetX() const;
+    const int& GetY() const;
+
+    const ManagerShips& GetManager() const;
 
 
-    const std::size_t& GetX() const;
+    void put_ships();
 
+
+    void shoot(Coords coord);
+    bool check_point(Coords coord);
+    
+    void print() const;
 
 
 };
